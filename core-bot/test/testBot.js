b3a6cc0e20c5232ee9cd28dcbad22794cbe1c084
rezvan <rezvan.mahdavi@gmail.com>
2017-10-22 21:49:46 -0400

    change mock data and testBot.js

--- core-bot/test/testBot.js
+++ core-bot/test/testBot.js
@@ -2,14 +2,51 @@ var chai = require("chai");
 var expect = chai.expect;
 var nock = require("nock");
 
-var main = require("../bot.js");
+// const Botmock = require('botkit-mock');
+// const fileBeingTested = require("./indexController");
+
+var main = require("../service.js");
 
 // Load mock data
-var data = require("../mock.json")
+var data = require("../mock.json");
 
-describe('testBot', function(){
+describe('testService', function(){
 
   // MOCK SERVICE
-  var mockService = nock("https://api.api.ai/v1/")
+  var mockService = nock("https://api.digitalocean.com/v2/")
   .persist()
+  .get("/new_VM")
+  .reply(200, JSON.stringify(data) );
+
+  describe('#createVirtualMachine()', function(){
+
+    //afterEach(function () {
+      //this.controller.shutdown();
+    //});
+
+    //beforeEach(function () {
+      //this.userInfo = {
+        //slackId: 'user123',
+        //channel: 'channel123',
+      //};
+
+      //this.controller = Botmock({
+        //stats_optout: true,
+        //debug: false,
+      //});
+
+      //this.bot = this.controller.spawn({
+        //type: 'slack',
+      //});
+    //});
+
+   	it('should create a VM', function() {
+
+      main.createVirtualMachine(bot, message, response).then(function (results)
+      {
+        //expect(results).to.have.property("IP");
+        console.log(result);
+      });
+    });
+  });
 });
